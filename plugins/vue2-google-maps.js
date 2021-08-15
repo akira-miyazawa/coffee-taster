6f7396740237d9c7d56058051f7beae68b55704e
akira-miyazawa <gongzezhang72@gmail.com>
2021-08-15 18:14:12 +0900

    設定ファイルの削除

--- plugins/vue2-google-maps.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import * as VueGoogleMaps from 'vue2-google-maps'
-
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyBpsJDdLtpXQbJ3AEhhdbAd_GUnW-A1Enw',
-    libraries: 'places', 
-  },
-})
