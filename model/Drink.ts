63818c3031a4edb621e75c653e20b5a0e6174b92
akira-miyazawa <gongzezhang72@gmail.com>
2021-09-11 17:30:31 +0900

    Drinkモデルを修正

--- model/Drink.ts
+++ model/Drink.ts
@@ -7,12 +7,14 @@ export class Drink {
   public readonly name: string;
   /** HOT or ICE */
   public readonly status: DrinkStatus;
+  /** お気に入り度 */
+  public readonly score: number = 3;
   /** テイスト */
-  public readonly score: CoffeeTasteScore;
+  public readonly coffeeTastescore: CoffeeTasteScore;
   /** コーヒー豆の情報 */
   public readonly bean: Bean;
 
-  constructor(name: string, status: DrinkStatus, score: CoffeeTasteScore, bean: Bean) {
+  constructor(name: string, status: DrinkStatus, score: number, coffeeTastescore: CoffeeTasteScore, bean: Bean) {
     if (name == null || name === '') {
       throw new Error('ドリンク名は必須です');
     }
@@ -20,11 +22,18 @@ export class Drink {
       throw new Error('ドリンクのHOT or ICEは必須です');
     }
     if (score == null) {
-      throw new Error('ドリンクのスコアは必須です');
+      throw new Error('ドリンクのお気に入り度は必須です');
+    }
+    if (score > 0 && score <= 5) {
+      throw new Error('ドリンクのお気に入り度は, 1以上５以下の値です')
+    }
+    if (coffeeTastescore == null) {
+      throw new Error('コーヒーのスコアは必須です');
     }
     this.name = name;
     this.status = status;
     this.score = score;
+    this.coffeeTastescore = coffeeTastescore;
     this.bean = bean;
   }
 }
