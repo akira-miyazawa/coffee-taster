40703f8e2098162e8da14a48b73f36d6cbe133f2
akira-miyazawa <gongzezhang72@gmail.com>
2021-11-23 20:44:02 +0900

    修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - cafe-taster',
-    title: 'cafe-taster',
+    titleTemplate: '%s - coffee-taster',
+    title: 'cofee-taster',
     htmlAttrs: {
       lang: 'en'
     },
