e597c70c34edc41bb9b9b8ef128eca4556b44a78
akira-miyazawa <gongzezhang72@gmail.com>
2021-11-23 22:04:17 +0900

    gitignoreの修正

--- plugins/vue2-google-maps.ts
+++ plugins/vue2-google-maps.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import * as VueGoogleMaps from 'vue2-google-maps';
-import { googleMapConfig } from '@/.firebase/config';
+import { googleMapConfig } from '@/firebase/config';
 
 Vue.use(VueGoogleMaps, {
   load: googleMapConfig
